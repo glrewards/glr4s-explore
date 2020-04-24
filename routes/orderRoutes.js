b3a8eacdd74bfdb36f8221f836a2026b50de1904
joglrewards <john@greatlittlerewards.co.uk>
2020-04-24 08:55:16 +0100

    delete working with api

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -105,70 +105,24 @@ module.exports = app => {
     });
 */
   app.put("/api/orders/deletelines/:centreId/:studentId", async (req, res) => {
-    try {
-      logger.info("put myorder received");
-      let studentTotal = 0; //used in forEAch loop
-      let studentId = req.params.studentId;
-      let lines = req.body;
-      let centre = req.params.centreId;
-      let orders = await Order.find({
-        _learningCentreId: centre,
-        fulfillStatus: "unfulfilled"
-      });
-      let cab = await Cabinet.findOne({ _learningCentreId: centre });
-      //order has an array of line items we want to delete the ones where we have an ID
-      //provided in the req.body array of IDs
-      //only expecting one so check
-      if (orders.length > 1) {
-        throw new Error({ code: 404, message: "too many open orders found" });
+    logger.debug("Received request here ", req.params);
+        //TODO: this url needs to be cleaned up
+    let url = keys.glrAPIGateway + keys.glrAPIOrder + "/deleteItems";
+    let options = {
+      headers: {
+        "X-API-KEY": keys.glrAPIGatewayKey,
+        "centreid": req.params.centreId,
+        "userid": req.params.studentId
       }
-      let order = orders[0];
-      logger.info("removing lineitem");
-
-      await lines.forEach(line => {
-        // TODO: All this needs to be in a transaction OR we refactor so that all orders
-        //  are just subdocuments of the cabinet itself. This might be the right way to
-        //  but remember a document can only get to 16MB max
-
-        //TODO: now we also need to repopulate the cabinet with the returned item
-        // a line can contain a quantity of items so we need to use this to add
-        // back onto the related rewardItem's count
-        let quantity = line.quantity;
-        //find the reward item
-        let reward = cab.shelves.map(shelf => {
-          return shelf.rewardItems.map(item => {
-            if (
-              JSON.stringify(item._id) ===
-              JSON.stringify(order.lineItems.id(line)._rewardId)
-            ) {
-              item.count = item.count + order.lineItems.id(line).quantity;
-              studentTotal +=
-                order.lineItems.id(line).quantity *
-                order.lineItems.id(line).glrpoints;
-              return item.count;
-            }
-          });
-        });
-
-        //reward.count += order.lineItems.id(line).quantity;
-        cab.markModified("shelves");
-        //for each line I need to refund the points to the student
-        // here I need to calculate a running total
-        order.lineItems.id(line).remove();
-      });
-      logger.debug("removed from array now saving document");
-      logger.debug("student points to refund: ", studentTotal);
-      let student = await Student.findById(studentId);
-      student.currentPoints += studentTotal;
-      student.markModified("student.currentPoints");
-      await order.save();
-      await cab.save();
-      await student.save();
-      logger.debug("saved document sending new order as a response");
-      res.send("successful operation");
+    };
+    try {
+      logger.info("calling axios: " + url);
+      const axiosResponse = await axios.put(url, req.body, options);
+      const data = axiosResponse.data;
+      res.send(data);
     } catch (err) {
-      logger.error(err);
-      throw err;
+      logger.error("error getting order: ", err);
+      res.send(err);
     }
   });
 
