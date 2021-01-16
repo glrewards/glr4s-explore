8e8f39466a87a04eff8e136074c86ac71780e09f
glrewards <john@greatlittlerewards.co.uk>
2021-01-16 16:08:33 +0000

    webhook is updating existing quantities now

--- routes/shopifyWebHookRoutes.js
+++ routes/shopifyWebHookRoutes.js
@@ -23,6 +23,27 @@ const logger = winston.createLogger({
   transports: [new winston.transports.Console()]
 });
 
+async function updateCabinet(cabinet){
+  logger.log({level: 'info', message: 'trying to update cabonet via api'});
+  let url = keys.glrAPIGateway + keys.glrAPICabinet;
+  let options = {
+    //tells axios not to throw an error for codes less than 404
+    validateStatus: function(status) {
+      return status < 500;
+    },
+    params: {
+      centre: cabinet._learningCentreId
+    },
+    headers: {
+      "X-API-KEY": keys.glrAPIGatewayKey
+    }
+  };
+  logger.log({ level: "debug", message: "calling axios: " + url });
+    const axiosResponse = await axios.put(url, cabinet,options);
+    logger.log({level: 'info', message: 'finished call to PUT api'});
+    logger.log({level: 'debug',message: 'response was: ', state: axiosResponse});
+    return axiosResponse;
+}
 async function createRewardObject(line) {
   /* a line item contains some but not all the data we need. It does not contain the metafields and these
     meant to be the means by which we allocate a product to a shelf. Also rewards only contain objectid references to
@@ -45,7 +66,11 @@ async function createRewardObject(line) {
       }
     };
     const product = await products.findOne(query, options);
-    logger.log({level: 'debug', message: "shopify product found",state: product});
+    logger.log({
+      level: "debug",
+      message: "shopify product found",
+      state: product
+    });
 
     //all items via this route will always be glr issued. So we need to look up the object Id for glr issuer
     const issuers = mclient.db().collection("issuer");
@@ -63,45 +88,51 @@ async function createRewardObject(line) {
   }
 }
 
-async function processLineItems(cabinet, lines){
-  try{
+async function processLineItems(cabinet, lines) {
+  try {
     for (const item of lines) {
       let reward = await createRewardObject(item);
-      logger.log({level: 'info', message: 'this is the created reward', state: reward});
+      logger.log({
+        level: "info",
+        message: "this is the created reward",
+        state: reward
+      });
       //use a case statement to add the reward the to the correct shelf
       switch (reward.points) {
         case 25:
-          logger.log({level: 'info', message: "switch case 25"});
+          logger.log({ level: "info", message: "switch case 25" });
           delete reward.points;
           cabinet.shelves[0].rewardItems.push(reward);
           break;
         case 50:
-          logger.log({level: 'info', message: "switch case 50"});
+          logger.log({ level: "info", message: "switch case 50" });
           delete reward.points;
           cabinet.shelves[1].rewardItems.push(reward);
           break;
         case 75:
-          logger.log({level: 'info', message: "switch case 75"});
+          logger.log({ level: "info", message: "switch case 75" });
           delete reward.points;
           cabinet.shelves[2].rewardItems.push(reward);
           break;
         case 100:
-          logger.log({level: 'info', message: "switch case 100"});
+          logger.log({ level: "info", message: "switch case 100" });
           delete reward.points;
           cabinet.shelves[3].rewardItems.push(reward);
           break;
         case 200:
-          logger.log({level: 'info', message: "switch case 200"});
+          logger.log({ level: "info", message: "switch case 200" });
           delete reward.points;
           cabinet.shelves[4].rewardItems.push(reward);
           break;
         default:
-          logger.log({level: 'debug', message: "did not match in the switch statement as expected", state: reward});
+          logger.log({
+            level: "debug",
+            message: "did not match in the switch statement as expected",
+            state: reward
+          });
       }
     }
-  }catch (e){
-
-  }
+  } catch (e) {}
 }
 function createSkeletonCabinetSimple(centreId, centreName) {
   const shelves = [
@@ -159,6 +190,73 @@ function createSkeletonCabinetSimple(centreId, centreName) {
   return newCab;
 }
 
+function updateExistingCabinet(cabinet, lineItems) {
+  try {
+    logger.log({
+      level: "info",
+      message: "started to update existing Cabinet"
+    });
+    logger.log({
+      level: "debug",
+      message: "updateExistingCabinet: parameters passed: ",
+      state: { cabinet, lineItems }
+    });
+    //ensure we have been passed some objects
+    if (!lineItems || !cabinet) {
+      throw { status: 404, message: "the passed parameters were not valid" };
+    }
+    //start the process
+    logger.log({
+      level: "info",
+      message: "starting to loop through the line items"
+    });
+    // if I find the item then increment the count by the quantity on the line item
+
+    cabinet.shelves.forEach(shelf => {
+      shelf.rewardItems.forEach(reward => {
+        //see if the item is in the list of line items
+        logger.log({
+          level: "debug",
+          message: "reward product Id: " + reward._shopifyProduct.id
+        });
+        let found = lineItems.find(line => {
+          logger.log({
+            level: "debug",
+            message: "comparing product Ids: ",
+            state: {
+              rewardId: reward._shopifyProduct.id,
+              lineId: line.product_id
+            }
+          });
+          if (reward._shopifyProduct.id === line.product_id.toString()){
+            logger.log({
+              level: "info",
+              message: "found a match between lineItems and cabinet rewards"
+            });
+            logger.log({
+              level: "debug",
+              message:
+                  "reward:" + reward._shopifyProduct.title + " count was " + reward.count
+            });
+            reward.count += line.quantity;
+            logger.log({
+              level: "debug",
+              message:
+                  "incremented reward:" + reward._shopifyProduct.title + " to " + reward.count
+            });
+          }
+
+        });
+
+        //if the item matches increment the count by the lineitem quantity
+      });
+    });
+    //at this stage existing rewards have been incremented so we can do update the cabinet
+
+  } finally {
+  }
+}
+
 async function storeHook(payload) {
   try {
     await mclient.connect();
@@ -195,8 +293,9 @@ module.exports = app => {
         };
         logger.log({ level: "debug", message: "calling axios: " + url });
         const axiosResponse = await axios.get(url, options);
+        //remember that the API populates the shopify product data when sending us the cabinet data
+        //so we have what we need to match lines with rewards.
         logger.log({ level: "debug", message: axiosResponse.data });
-
         // if no cabinet was found this is not an error. It should just mean that we do not have a cabinet
         // for the centre so we need to create one.
 
@@ -216,7 +315,7 @@ module.exports = app => {
             req.centreName
           );
 
-           await processLineItems(newCabinet,req.body.line_items);
+          await processLineItems(newCabinet, req.body.line_items);
           //console.log(newCabinet);
 
           //now need to populate
@@ -244,6 +343,10 @@ module.exports = app => {
           /*
             Now we need to push the reward onto the appropriate shelf object
              */
+        } else {
+          const existingCabinet = axiosResponse.data;
+          updateExistingCabinet(existingCabinet, req.body.line_items);
+          await updateCabinet(existingCabinet);
         }
         res.send({ code: 200 }); // return 200 whatever happens to  shopify
       } catch (e) {
@@ -251,7 +354,7 @@ module.exports = app => {
           level: "debug",
           message: "still landing in catch block"
         });
-        logger.error(e.message);
+        logger.log({level: 'error', message: e.message, state: e});
         res.send({ code: 401 });
       }
     }
