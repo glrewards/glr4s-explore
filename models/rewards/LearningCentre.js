9330f137ca2480623f2219110aea851fb9872490
glrewards <john@greatlittlerewards.co.uk>
2021-03-31 08:42:46 +0100

    added address and geolocation to LearningCentre.js

--- models/rewards/LearningCentre.js
+++ models/rewards/LearningCentre.js
@@ -4,8 +4,19 @@ const { Schema } = mongoose;
 
 const learningCentreSchema = new Schema({
     name: { type: String, required: true },
-    location: {type: String},
-    schema_version: {type: String, default: 'v2'}
+    address: {postCode: String, Address1: String, Address2: String, City: String, County: String, Country: String
+    },
+    location: {
+        type:{
+            type: String,
+            enum: ['Point'],
+            required:true
+        },
+        coordinates:{
+            type: [Number],
+            required:true
+        }
+    },
+    schema_version: {type: String, default: 'v3'}
 });
-
 mongoose.model("LearningCentre", learningCentreSchema);
