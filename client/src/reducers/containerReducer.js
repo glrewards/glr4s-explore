66d4903c7d2d513173482bcd38ccdf4ba47e6e3f
glrewards <john@greatlittlerewards.co.uk>
2021-05-07 09:32:48 +0100

    containerReducer.js started with simple fetching flag

--- client/src/reducers/containerReducer.js
+++ client/src/reducers/containerReducer.js
@@ -1,10 +1,22 @@
 import {CONTAINER_RECEIVED_MEMBERS, CONTAINER_REQUEST_MEMBERS, CONTAINER_MEMBER_CLICKED} from "../actions/containerActions";
+import order from "./orderReducer";
 
-export default function(state = null, action) {
+ function container(state = {
+    isFetching: false,
+    members: []
+}, action) {
     switch (action.type) {
         case CONTAINER_REQUEST_MEMBERS:
-            return action.payload || false;
+            return Object.assign({}, state, {
+                isFetching: true
+            });
+        case CONTAINER_RECEIVED_MEMBERS:
+            return Object.assign({}, state, {
+                isFetching: false
+            });
         default:
             return state;
     }
 }
+
+export default container;
