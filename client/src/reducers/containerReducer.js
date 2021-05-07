40d0a93a04fca7f7c638ad5442413e2d237d5044
glrewards <john@greatlittlerewards.co.uk>
2021-05-07 09:41:25 +0100

    containerReducer.js add teh selected flag to each member

--- client/src/reducers/containerReducer.js
+++ client/src/reducers/containerReducer.js
@@ -1,22 +1,36 @@
-import {CONTAINER_RECEIVED_MEMBERS, CONTAINER_REQUEST_MEMBERS, CONTAINER_MEMBER_CLICKED} from "../actions/containerActions";
+import {
+  CONTAINER_RECEIVED_MEMBERS,
+  CONTAINER_REQUEST_MEMBERS,
+  CONTAINER_MEMBER_CLICKED
+} from "../actions/containerActions";
+
 import order from "./orderReducer";
 
- function container(state = {
+function container(
+  state = {
     isFetching: false,
     members: []
-}, action) {
-    switch (action.type) {
-        case CONTAINER_REQUEST_MEMBERS:
-            return Object.assign({}, state, {
-                isFetching: true
-            });
-        case CONTAINER_RECEIVED_MEMBERS:
-            return Object.assign({}, state, {
-                isFetching: false
-            });
-        default:
-            return state;
-    }
+  },
+  action
+) {
+  switch (action.type) {
+    case CONTAINER_REQUEST_MEMBERS:
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    case CONTAINER_RECEIVED_MEMBERS:
+        //loop through the payload and add a selected flag to each member
+        let members = action.payload;
+        members.forEach( member => {
+            member['selected'] = false;
+        });
+      return Object.assign({}, state, {
+        isFetching: false,
+        members: members
+      });
+    default:
+      return state;
+  }
 }
 
 export default container;
